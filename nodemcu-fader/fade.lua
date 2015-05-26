349c9dea2decd883e328debec7a9f940fc029ab2
makefu <github@syntax-fehler.de>
2015-05-27 01:38:06 +0200

    fade.lua: not using loop.lua anymore

--- nodemcu-fader/fade.lua
+++ nodemcu-fader/fade.lua
@@ -1,16 +1,23 @@
-local l=require('loop').new(1)
+local tmr = tmr
+local tmrid=1
 local buffer=""
 local pin=3
+
 local function fade_pixel(to_array,fader_delay,fader_steps)
   local from_array=buffer
   local NumLEDs = #to_array
+  local current_step=1
   local step_delay = fader_delay / fader_steps
-
-
-  local function run_fade(current_step)
+  
+  local function run_fade()
+    
+    if current_step >= fader_steps then
+        -- stop self
+        tmr.stop(tmrid)
+    end
     local previous_weight
     local current_weight
-    local rgb_buffer
+    
     -- this only implements logarithmic decay
     previous_weight = 100 * (fader_steps - current_step);
     current_weight = 100 * (current_step - 1);
@@ -21,20 +28,20 @@ local function fade_pixel(to_array,fader_delay,fader_steps)
     for i=1,NumLEDs,3 do
       if not from_array:byte(i) then from_array =from_array.. string.char(0,0,0) end
 
-      local r = ( ( (to_array:byte(i) * current_weight)+
+      local g = ( ( (to_array:byte(i) * current_weight)+
                 (from_array:byte(i) * previous_weight) ) / fader_weight )
-      local g = ( ( (to_array:byte(i+1) * current_weight)+
+      local r = ( ( (to_array:byte(i+1) * current_weight)+
                 (from_array:byte(i+1) * previous_weight) ) / fader_weight )
       local b = ( ( (to_array:byte(i+2) * current_weight)+
                 (from_array:byte(i+2) * previous_weight) ) / fader_weight )
-      buffer = buffer .. string.char(r,g,b)
+      buffer = buffer .. string.char(g,r,b)
     end
-    rgb_buffer=buffer..string.char(0,0,0)
-    ws2812.writergb(pin,rgb_buffer)
+    ws2812.write(pin,buffer)
+    current_step = current_step + 1
   end
 
-  l:stop()
-  l:loop(1,fader_steps,1,step_delay,run_fade)
+  tmr.stop(tmrid)
+  tmr.alarm(tmrid,step_delay,1,run_fade)
 
 end
 local function get_buffer()
@@ -46,6 +53,7 @@ end
 local function get_ledpin()
     return pin
 end
+
+
 return {fade=fade_pixel,buffer=get_buffer,
-        looper=loop,
         set_ledpin=set_ledpin,ledpin=get_ledpin}
