fe629ae7071651ee33039b42451b1fdbe0156a8e
makefu <github@syntax-fehler.de>
2015-06-16 22:54:10 +0200

    start the server

--- nodemcu-fader/init.lua
+++ nodemcu-fader/init.lua
@@ -1,7 +1,7 @@
---dofile('wifi.lc')
---dofile('fadesrv.lc')
+dofile('wifi.lc')
+dofile('fadesrv.lc')
 
-x = require('wheel')
+--x = require('wheel')
 --tmr.alarm(1,50,1,function () x.wheel() end)
 
 -- ws2812.writergb(1,"DICKBUTT")
