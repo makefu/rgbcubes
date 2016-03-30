4c04803d9cc4a45f27d89f67b5f489aa34766fc9
makefu <github@syntax-fehler.de>
2016-03-30 23:45:03 +0200

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,18 +1,33 @@
 { pkgs ? import <nixpkgs> {} }:
 let
   # TODO: from <stockholm>
+  nodemcu-uploader-next = pkgs.python2Packages.buildPythonPackage rec {
+      name = "nodemcu-uploader-${version}";
+      version = "0.3.next";
+      propagatedBuildInputs = with pkgs;[
+        python2Packages.pyserial
+      ];
+      src = pkgs.fetchFromGitHub {
+        owner = "kmpm";
+        repo = "nodemcu-uploader";
+        rev = "next";
+        sha256 = "17nx7l7kqw2dpp99p2nwxj1cgamv7xm1a8r6hlpgdins2hnggplv";
+      };
+      doCheck = false;
+  };
   nodemcu-uploader = pkgs.python2Packages.buildPythonPackage rec {
       name = "nodemcu-uploader-${version}";
-      version = "0.2.2";
+      version = "0.3.0";
       propagatedBuildInputs = with pkgs;[
         python2Packages.pyserial
       ];
       src = pkgs.fetchurl {
         url = "https://pypi.python.org/packages/source/n/nodemcu-uploader/nodemcu-uploader-${version}.tar.gz";
-        sha256 = "090giz84y9y3idgifp0yh80qqyv2czv6h3y55wyrlgf7qfbwbrvn";
+        sha256 = "1rjsfnj3gawvdjdj2nngizpxln5d71gbx1l141520569p077mm65";
       };
       doCheck = false;
   };
+
   esptool = pkgs.python2Packages.buildPythonPackage rec {
       name = "esptool-${version}";
       version = "0.1.0";
@@ -37,7 +52,7 @@ in pkgs.stdenv.mkDerivation rec {
     gawk
     bash
     # platformio
-    nodemcu-uploader
+    nodemcu-uploader-next
     esptool
   ];
     shellHook =''
